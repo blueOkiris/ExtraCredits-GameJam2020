e3e2d3585acf09b25bf09d8046aa9809028a1051
Dylan Turner <dylan.turner@ni.com>
2020-08-17 10:32:31 -0500

    Ported program code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <engine.hpp>
+#include <res/settings.hpp>
 
-int main(int argc, char **args) {
-    return 0;
+namespace gamejame {
+    void safelyDisposeMusic();
+
+    int main(int argc, char **args) {
+        auto engine = engine::Engine::getInstance();
+        engine.windowThread.join();
+
+        safelyDisposeMusic();
+
+        return 0;
+    }
+
+    void safelyDisposeMusic() {
+        settings::bgMusic.stop();
+        settings::popSfx.stop();
+        settings::bgMusic.dispose();
+        settings::popSfx.dispose();
+
+        settings::bgMusicFile.dispose();
+        settings::popSfx.dispose();
+    }
 }
