c5159f695f1681a4b8b1d690ee50ed9ebb0c35b8
Dylan Turner <dylan.turner@ni.com>
2020-08-17 10:12:35 -0500

    Moved C# code out of repo

---
+++ csharp-reference/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace engine {
+    class Program {
+        public static void Main(string[] args) {
+            var engine = Engine.getInstance();
+            engine.windowThread.Join();
+
+            SafelyDisposeMusic();
+        }
+
+        private static void SafelyDisposeMusic() {
+            Settings.BgMusic.Stop();
+            Settings.PopSfx.Stop();
+            Settings.BgMusic.Dispose();
+            Settings.PopSfx.Dispose();
+
+            Settings.BgMusicFile.Dispose();
+            Settings.PopSfxFile.Dispose();
+        }
+    }
+}
